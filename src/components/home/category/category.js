e33d87548baf7c7f4cc9df1117657af01fa703ff
Mohammed-Ashraful <mr.ashraful2020@gmail.com>
2022-09-10 11:11:29 +0600

    updated category section

--- src/components/home/category/category.js
+++ src/components/home/category/category.js
@@ -8,8 +8,9 @@ import img5 from "../../../assets/category/pet.png";
 const Category = memo(() => {
   return (
     <>
-      <div className="mb-6 bg-gradient-to-b from-neutral-300 via-stone-100 to-white md:-mt-56">
-        <div className="md:mx-32">
+      <div className="mb-6 m-2 bg-gradient-to-b from-neutral-300 via-stone-100 to-white lg:-mt-56 md:-mt-32 -mt-12 ">
+        <div className="lg:mx-32">
+        <div className="mx-auto">
           <div className="items-center justify-center md:flex">
             <Product id={1} title="Computers & Accessories" img={img1} />
             <Product id={2} title="Electronics " img={img2} />
@@ -21,17 +22,18 @@ const Category = memo(() => {
           </div>
         </div>
       </div>
+      </div>
     </>
   );
 });
 
 export default Category;
 
-function Product({ img, title, price, rating }) {
+function Product({ img, title}) {
   return (
-    <div className="z-10 m-3 rounded-md bg-white px-8 py-4 text-left shadow-2xl">
-      <h1 className="text-2xl font-semibold">{title}</h1>
-      <img src={img} alt="" srcSet="" />
+    <div className=" z-50 mx-auto  w-2/4 m-3 rounded-md bg-white px-3 py-2 lg:px-8 lg:py-4 text-left shadow-2xl">
+      <h1 className="lg:text-2xl font-semibold">{title}</h1>
+      <img className="" src={img} alt="" srcSet="" />
       <p className="  text-sm text-blue-500 underline">Explore now</p>
     </div>
   );
